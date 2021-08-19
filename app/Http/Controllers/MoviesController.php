233c4f0f24ccf7f07a54396483b9125c2902adf6
Yoga Baskoro <baskoroyoga40@gmail.com>
2021-08-19 11:56:00 +0700

    Looping each of popular movies to index.view

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -11,8 +11,18 @@ class MoviesController extends Controller
     {
         $popularMovies = Http::withToken(config('services.tmdb.token'))
             ->get('https://api.themoviedb.org/3/movie/popular')
-            ->json();
-        dd($popularMovies);
-        return view('index');
+            ->json()['results'];
+        $genresMovies = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/genre/movie/list')->json()['genres'];
+        $genres = collect($genresMovies)->mapWithKeys(function ($genre) {
+            return [$genre['id'] => $genre['name']];
+        });
+        // dd($genres);
+        dump($popularMovies);
+        $data = [
+            'popularMovies' => $popularMovies,
+            'genres' => $genres
+        ];
+        return view('index', $data);
     }
 }
